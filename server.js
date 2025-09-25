ed6f3dce8afa0a9d4e9a71fbd886d5e0614ea9b1
sabrine-khalifa <sabrinekhalifa12@gmail.com>
2025-09-25 04:17:56 +0200

    comment backend cloudinary

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+console.log("server est chargée");
 
 // Connexion DB
 mongoose.connect(process.env.MONGO_URI)
