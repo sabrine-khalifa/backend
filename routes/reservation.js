bd70e5028dbe3424015278e42ab472a0ad663055
sabrine-khalifa <sabrinekhalifa12@gmail.com>
2025-12-19 20:16:14 +0100

    add service

--- routes/reservation.js
+++ routes/reservation.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const Reservation = require('../models/Reservation'); // ✅ le bon modèle
 
+const authMiddleware = require("../middlewares/auth");
+
 // Route pour compter les réservations d'un service
 router.get('/count/:serviceId', async (req, res) => {
   try {
@@ -16,19 +18,26 @@ router.get('/count/:serviceId', async (req, res) => {
 });
 
 // Récupérer toutes les réservations d’un utilisateur
-router.get('/user/:userId', async (req, res) => {
+router.get("/user/:userId", authMiddleware, async (req, res) => {
   try {
-    const { userId } = req.params;
-    const reservations = await Reservation.find({ utilisateur: userId })
-      .populate("service")   // pour avoir les infos du service réservé
-      .populate("utilisateur", "nom email"); // infos utilisateur si besoin
+    const reservations = await Reservation.find({
+      utilisateur: req.params.userId
+    })
+      .populate({
+        path: "service",
+        select: "titre duree typePrestation creditsProposes dateService",
+        populate: {
+          path: "createur",
+          select: "name prenom photo"
+        }
+      })
+      .sort({ createdAt: -1 });
 
     res.json(reservations);
   } catch (err) {
-    console.error(err);
+    console.error("❌ Erreur réservations :", err);
     res.status(500).json({ msg: "Erreur serveur" });
   }
 });
-
 
 module.exports = router;
